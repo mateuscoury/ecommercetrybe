93f730c16a453cc03a2c8db61af5790d299f275d
mateuscoury <mateuscoury@gmail.com>
2021-03-22 10:04:01 -0300

    Updates

---
+++ precache-manifest.5e409459ed70a36e23709bb0049fce65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654544ebf140ab100c38c2878ee42768",
+    "url": "/frontend-onlinestore/index.html"
+  },
+  {
+    "revision": "3f4e13e0db01a62da83b",
+    "url": "/frontend-onlinestore/static/css/main.99845a48.chunk.css"
+  },
+  {
+    "revision": "fd820d62008a7c3ec45d",
+    "url": "/frontend-onlinestore/static/js/2.a4f002ad.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-onlinestore/static/js/2.a4f002ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4e13e0db01a62da83b",
+    "url": "/frontend-onlinestore/static/js/main.71b22fca.chunk.js"
+  },
+  {
+    "revision": "470979f94ddb0f036cd5",
+    "url": "/frontend-onlinestore/static/js/runtime-main.10759ea3.js"
+  }
+]);
