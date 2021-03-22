85c9b11b2a3a0573826f7217da6cb4d6657cc5da
mateuscoury <mateuscoury@gmail.com>
2021-03-22 20:56:23 -0300

    Updates

--- precache-manifest.50a5eb59a2bf7fb2129139854b518b08.js
+++ precache-manifest.50a5eb59a2bf7fb2129139854b518b08.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fd8c052541d6f3a40cf84afef1d5e1e6",
+    "revision": "1bfef4a7dc73dd73842dd82bcc20e2dc",
     "url": "/ecommercetrybe/index.html"
   },
   {
-    "revision": "c45d3a7a491403eac8d1",
+    "revision": "cb08a58f5b0873492c64",
     "url": "/ecommercetrybe/static/css/main.99845a48.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ecommercetrybe/static/js/2.a4f002ad.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c45d3a7a491403eac8d1",
-    "url": "/ecommercetrybe/static/js/main.a5bcee6c.chunk.js"
+    "revision": "cb08a58f5b0873492c64",
+    "url": "/ecommercetrybe/static/js/main.e3aada1d.chunk.js"
   },
   {
     "revision": "fc189f9088af24b2bb10",
