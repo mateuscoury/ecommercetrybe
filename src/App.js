e2ce19acdd07f1316bd95b82878195bb6b0e6355
mateuscoury <mateuscoury@gmail.com>
2021-03-22 20:47:50 -0300

    sadsa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CartPage from './components/CartPage';
 import './App.css';
 import './services/api';
@@ -130,7 +130,7 @@ class App extends React.Component {
   render() {
     const { categories, products, productsOnCart, totalItems } = this.state;
     return (
-      <HashRouter  basename={process.env.PUBLIC_URL}>
+      <BrowserRouter  basename={process.env.PUBLIC_URL}>
         <TopNavBar cartSize={ totalItems } />
         <Switch>
           <Route
