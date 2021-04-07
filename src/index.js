3d2f9ef281005c3865f563461a56f73bdb60116e
mateuscoury <mateuscoury@gmail.com>
2021-04-07 09:08:45 -0300

    asdasdas

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  
-    <App />,
+  <HashRouter>
+
+    <App />
+  </HashRouter>,
   
   document.getElementById('root'),
 );
